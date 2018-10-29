eec7b18d6a6ffadb492e7b2de73c52b8db38d8c0
Brandon Cook <bgcook@lbl.gov>
2018-10-29 13:23:42 -0700

    only lint md files

--- util/lint-changes.sh
+++ util/lint-changes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-for f in $(git diff master --name-only); do   
-    markdownlint $f
+for f in $(git diff master --name-only); do
+    if [ ${f: -3} == ".md" ]; then
+	markdownlint $f
+    fi
 done
