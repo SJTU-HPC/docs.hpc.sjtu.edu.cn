14c491744908a1a6a34de350036df22b76601634
Jianwen WEI <weijianwen@gmail.com>
2020-08-13 23:05:41 +0800

    Setup the landing page.

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'Pi Documentation'
@@ -51,9 +50,38 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_material'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Pi Documentation',
+
+    # Set you GA account ID to enable tracking
+    'google_analytics_account': 'UA-XXXXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://project.github.io/project',
+
+    # Set the color and the accent color
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/SJTU-HPC/docs.hpc.sjtu.edu.cn',
+    'repo_name': 'SJTU HPC Docs',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
--- docs/index.rst
+++ docs/index.rst
@@ -1,20 +1,29 @@
-.. Pi Documentation documentation master file, created by
-   sphinx-quickstart on Thu Aug 13 22:18:30 2020.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to Pi Documentation's documentation!
-============================================
-
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
-
-
-
-Indices and tables
+Pi超算集群用户文档
 ==================
 
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+`docs <http://docs.hpc.sjtu.edu.cn/>`_ 是上海交通大学 Pi2.0 超级计算集群用户文档，为用户提供快速上手指导和问题解答。如果想获取更多 hpc 团队的动态和信息，请访问 `hpc <https://hpc.sjtu.edu.cn/>`_ 。   
+
+.. caution:: *超算平台禁止**运行军工项目等涉密计算任务。*
+
+.. tip:: 该文档通过 `github仓库 <https://github.com/SJTU-HPC/docs.hpc.sjtu.edu.cn>`__\ 维护。如果您有什么建议或者意见，欢迎提交修改。 
+
+快速链接
+--------
+
+1. `账号申请 <accounts/apply.md>`_
+2. `密码 <accounts/passwords.md>`_
+3. `远程登陆 <login/SSH.md>`_
+4. `远程桌面 <studio/basic.md>`_
+5. `作业提交 <job/slurm.md>`_
+6. `应用软件 <application/applications.md>`_
+7. `使用GNU套件编译代码 <application/GNU.md>`_
+8. `使用Intel套件编译代码 <application/Intel.md>`_
+9. `常见问题 <faq.md>`_
+
+.. toctree::
+   :maxdepth: 1
+   :caption: 账户
+
+   accounts/apply
+   
+
