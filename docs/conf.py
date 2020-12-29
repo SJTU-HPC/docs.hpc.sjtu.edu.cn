e54d801d9eae31ac175c4487ed21039c83d231c4
zephyr221 <zephyrlulu@gmail.com>
2020-12-29 13:59:24 +0800

    try adding favicon

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,7 @@ html_theme = "sphinx_material"
 # a list of builtin themes.
 #
 html_theme = 'sphinx_material'
+html_favicon = '../img/pi.ico'
 
 
 # Material theme options (see theme.conf for more information)
