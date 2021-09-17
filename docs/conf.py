b2e5e3857348c0885fdb02e5b59d5f50dfaa0ba3
hpcsjw <hpcsjw@login3.pi.sjtu.edu.cn>
2021-09-17 10:16:06 +0800

    update-mast-success

--- docs/conf.py
+++ docs/conf.py
@@ -60,9 +60,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_css_files = [
-	'css/app.css',
-]
+#html_css_files = [
+#	'css/app.css',
+#]
 
 # -- HTML theme settings -----------------------------------------------
 
