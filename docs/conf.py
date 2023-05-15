09cd71d1c2200807f8421445ca4e25c6951cd390
Jianwen WEI <weijianwen@gmail.com>
2023-05-15 17:17:11 +0800

    Re-enable sphinx-prompt.

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ record = '沪交ICP备20190201'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx-prompt',
     'sphinx_copybutton',
     'sphinx_substitution_extensions',
     'sphinx.ext.autosectionlabel',
