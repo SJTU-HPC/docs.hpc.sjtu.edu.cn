b3126e35063bdae97bbf482aab59c6f3748e021b
Jianwen WEI <weijianwen@gmail.com>
2024-04-08 15:44:45 +0800

    Comment HTML rendering language.

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh_CN'
+# language = 'zh_CN'
 html_search_language = 'zh'
 
 # List of patterns, relative to source directory, that match files and
